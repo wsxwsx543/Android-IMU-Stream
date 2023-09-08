7e1c7712256bf5162dd1a208633aea5ceb423b0b
wsxwsx543 <wusixuan543543@gmail.com>
2023-09-08 18:58:20 -0400

    init commit for smartphone end

---
+++ IMU_Data_Collection_Smartphone/app/src/main/java/com/example/imu_data_collection_smartphone/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.imu_data_collection_smartphone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
