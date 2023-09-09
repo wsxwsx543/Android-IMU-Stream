1fa5e0db385718c2048ea43b0ffea6bd813203cf
wsxwsx543 <wusixuan543543@gmail.com>
2023-09-09 16:46:11 -0400

    add index

--- IMU_Data_Collection_Smartwatch/app/src/main/java/com/example/imu_data_collection_smartwatch/IMUService.java
+++ IMU_Data_Collection_Smartwatch/app/src/main/java/com/example/imu_data_collection_smartwatch/IMUService.java
@@ -29,14 +29,19 @@ import java.io.ObjectOutputStream;
 import java.nio.ByteBuffer;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class IMUService extends Service implements SensorEventListener {
 
     private final String TAG = this.getClass().getName();
-    final static private String MY_UUID = "680ac785-9ed1-42cd-bdf0-76cf6b708f3b";
+    final static private String ACC_UUID = "680ac785-9ed1-42cd-bdf0-76cf6b708f3b";
+    final static private String GYRO_UUID = "e0740e55-4d73-4e97-b0fb-90afc0ebb980";
     private BluetoothAdapter bluetoothAdapter;
-    private final static int REQUEST_ENABLE_BT = 1;
-    private ConnectedThread connectedThread;
+
+    private ConnectedThread connectedThreadAcc;
+    private ConnectedThread connectedThreadGyro;
     public IMUService() {
     }
 
@@ -81,6 +86,11 @@ public class IMUService extends Service implements SensorEventListener {
             connectThread.start();
         }
 
+        sendingAccThread = new Thread(accSendRunnable);
+        sendingGyroThread = new Thread(gyroSendRunnable);
+        sendingAccThread.start();
+        sendingGyroThread.start();
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         linearAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
@@ -103,35 +113,69 @@ public class IMUService extends Service implements SensorEventListener {
 
 //    private SensorData sensorData = new SensorData();
     private final Object changeLock = new Object();
-    private float[] sensorData = new float[4];
+    // x, y, z, timestamp, name, index
+    int cnt = 0;
+    BlockingQueue<float[]> accBuffer = new LinkedBlockingQueue<>(1024);
+    BlockingQueue<float[]> gyroBuffer = new LinkedBlockingQueue<>(1024);
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        synchronized (changeLock) {
+//        synchronized (changeLock) {
 //            sensorData.timestamp = sensorEvent.timestamp;
+            float[] sensorData = new float[6];
             sensorData[0] = sensorEvent.values[0];
             sensorData[1] = sensorEvent.values[1];
             sensorData[2] = sensorEvent.values[2];
-            Log.d(TAG, "onSensorChanged: SensorChanged");
+            sensorData[3] = sensorEvent.timestamp;
+            sensorData[5] = cnt++;
+//            Log.d(TAG, "run: Receive updated sensor data:"
+//                + " index: " + sensorData[5]
+//                + " name: " + sensorData[4]
+//                + " timestamp: " + sensorData[3]
+//                + " x: " + sensorData[0]
+//                + " y: " + sensorData[1]
+//                + " z: " + sensorData[2]);
             if (sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-                sensorData[3] = 0;
-//                Log.d(TAG, sensorEvent.timestamp + " gyro: x: " + sensorEvent.values[0] + " y: "
-//                        + sensorEvent.values[1] + " z: " + sensorEvent.values[2]);
-
+                sensorData[4] = 0;
+                gyroBuffer.add(sensorData);
             } else if (sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-                sensorData[3] = 1;
-//                Log.d(TAG, sensorEvent.timestamp + " lin_acc: x: " + sensorEvent.values[0] + " y: "
-//                        + sensorEvent.values[1] + " z: " + sensorEvent.values[2]);
+                sensorData[4] = 1;
+                accBuffer.add(sensorData);
             }
-//            byte[] bytes = serialize(sensorData);
-//            if (bytes.length == 0) {
-//                Log.d(TAG, "onSensorChanged: serialize error!!!");
-//                return;
-//            }
-            if (connectedThread != null)
-            connectedThread.write(floatArrayToByteArray(sensorData));
-            Log.d(TAG, "onSensorChanged: write to socket");
-        }
+//            Log.d(TAG, "onSensorChanged: write to socket");
+//        }
     }
+
+    private Thread sendingAccThread;
+    private Thread sendingGyroThread;
+    private final Runnable accSendRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while (!sendingAccThread.isInterrupted()) {
+                try {
+                    float[] sensorData = accBuffer.take();
+                    if (connectedThreadAcc != null)
+                        connectedThreadAcc.write(floatArrayToByteArray(sensorData));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private final Runnable gyroSendRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while (!sendingGyroThread.isInterrupted()) {
+                try {
+                    float[] sensorData = gyroBuffer.take();
+                    if (connectedThreadGyro != null)
+                        connectedThreadGyro.write(floatArrayToByteArray(sensorData));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
 
     public static byte[] floatArrayToByteArray(float[] floatArray) {
         ByteBuffer buffer = ByteBuffer.allocate(floatArray.length * 4); // 4 bytes per float
@@ -147,24 +191,27 @@ public class IMUService extends Service implements SensorEventListener {
     }
 
     private class ConnectThread extends Thread {
-        private final BluetoothSocket mmSocket;
+        private final BluetoothSocket accSocket;
+        private final BluetoothSocket gyroSocket;
         private final BluetoothDevice mmDevice;
 
         @SuppressLint("MissingPermission")
         public ConnectThread(BluetoothDevice device) {
             // Use a temporary object that is later assigned to mmSocket
             // because mmSocket is final.
-            BluetoothSocket tmp = null;
+            BluetoothSocket tmp1 = null, tmp2 = null;
             mmDevice = device;
 
             try {
                 // Get a BluetoothSocket to connect with the given BluetoothDevice.
                 // MY_UUID is the app's UUID string, also used in the server code.
-                tmp = device.createRfcommSocketToServiceRecord(UUID.fromString(MY_UUID));
+                tmp1 = device.createRfcommSocketToServiceRecord(UUID.fromString(ACC_UUID));
+                tmp2 = device.createRfcommSocketToServiceRecord(UUID.fromString(GYRO_UUID));
             } catch (IOException e) {
                 Log.e(TAG, "Socket's create() method failed", e);
             }
-            mmSocket = tmp;
+            accSocket = tmp1;
+            gyroSocket = tmp2;
         }
 
         @Override
@@ -177,13 +224,15 @@ public class IMUService extends Service implements SensorEventListener {
                 // Connect to the remote device through the socket. This call blocks
                 // until it succeeds or throws an exception.
                 Log.d(TAG, "run: connecting");
-                mmSocket.connect();
+                accSocket.connect();
+                gyroSocket.connect();
                 Log.d(TAG, "run: Connected Successfully!");
             } catch (IOException connectException) {
                 Log.d(TAG, "run: cannot connect");
                 // Unable to connect; close the socket and return.
                 try {
-                    mmSocket.close();
+                    accSocket.close();
+                    gyroSocket.close();
                 } catch (IOException closeException) {
                     Log.e(TAG, "Could not close the client socket", closeException);
                 }
@@ -193,13 +242,15 @@ public class IMUService extends Service implements SensorEventListener {
             // The connection attempt succeeded. Perform work associated with
             // the connection in a separate thread.
             Log.d(TAG, "run: Connected Successfully!");
-            connectedThread = new ConnectedThread(mmSocket);
+            connectedThreadAcc = new ConnectedThread(accSocket);
+            connectedThreadGyro = new ConnectedThread(gyroSocket);
         }
 
         // Closes the client socket and causes the thread to finish.
         public void cancel() {
             try {
-                mmSocket.close();
+                accSocket.close();
+                gyroSocket.close();
             } catch (IOException e) {
                 Log.e(TAG, "Could not close the client socket", e);
             }
