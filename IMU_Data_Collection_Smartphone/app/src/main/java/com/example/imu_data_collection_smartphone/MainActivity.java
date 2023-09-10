f91f17987adeb81bf3005fa8ef5588e30b40c62f
wsxwsx543 <wusixuan543543@gmail.com>
2023-09-10 00:52:39 -0400

    add camera preview to the app

--- IMU_Data_Collection_Smartphone/app/src/main/java/com/example/imu_data_collection_smartphone/MainActivity.java
+++ IMU_Data_Collection_Smartphone/app/src/main/java/com/example/imu_data_collection_smartphone/MainActivity.java
@@ -8,12 +8,31 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.util.Range;
+import android.view.Surface;
+import android.view.TextureView;
 
 
 import com.jjoe64.graphview.GraphView;
@@ -23,7 +42,9 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.UUID;
 
@@ -76,6 +97,24 @@ public class MainActivity extends Activity implements AccListener, GyroListener 
         }
     }
 
+    TextureView textureView;
+    TextureView.SurfaceTextureListener surfaceTextureListener;
+    CameraManager cameraManager;
+    CameraDevice.StateCallback cam_stateCallback;
+    CameraDevice opened_camera;
+    Surface texture_surface;
+    CameraCaptureSession.StateCallback cam_capture_session_stateCallback;
+    CameraCaptureSession.CaptureCallback still_capture_callback;
+    CameraCaptureSession cameraCaptureSession;
+    CaptureRequest.Builder requestBuilder;
+    CaptureRequest.Builder requestBuilder_image_reader;
+    ImageReader imageReader;
+    Surface imageReaderSurface;
+    Bitmap bitmap;
+    CaptureRequest request;
+    CaptureRequest takephoto_request;
+
+
     @TargetApi(Build.VERSION_CODES.S)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +123,40 @@ public class MainActivity extends Activity implements AccListener, GyroListener 
 
         accGraph = findViewById(R.id.acc_graph);
         gyroGraph = findViewById(R.id.gyro_graph);
+        textureView = findViewById(R.id.texture_view);
+
+        surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                texture_surface = new Surface(textureView.getSurfaceTexture());
+                openCamera();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            }
+        };
+        textureView.setSurfaceTextureListener(surfaceTextureListener);
+        //B1. 准备工作：初始化ImageReader
+        imageReader = ImageReader.newInstance(1000, 1000, ImageFormat.JPEG, 2);
+        //B2. 准备工作：设置ImageReader收到图片后的回调函数
+        imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+            }
+        }, null);
+        //B3 配置：获取ImageReader的Surface
+        imageReaderSurface = imageReader.getSurface();
+
         initUI();
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -103,14 +176,15 @@ public class MainActivity extends Activity implements AccListener, GyroListener 
                 != PackageManager.PERMISSION_GRANTED) {
             Log.d(TAG, "onCreate: Request Permission");
             requestPermissions(new String[]{
-                    android.Manifest.permission.BLUETOOTH,
-                    android.Manifest.permission.BLUETOOTH_ADMIN,
-                    android.Manifest.permission.BLUETOOTH_CONNECT,
-                    android.Manifest.permission.WAKE_LOCK,
-                    android.Manifest.permission.FOREGROUND_SERVICE,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION},
-            2);
+                            android.Manifest.permission.BLUETOOTH,
+                            android.Manifest.permission.BLUETOOTH_ADMIN,
+                            android.Manifest.permission.BLUETOOTH_CONNECT,
+                            android.Manifest.permission.WAKE_LOCK,
+                            android.Manifest.permission.CAMERA,
+                            android.Manifest.permission.FOREGROUND_SERVICE,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    2);
         }
         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
         Log.d(TAG, "onCreate: pairedDevices length: " + pairedDevices.size());
@@ -123,9 +197,101 @@ public class MainActivity extends Activity implements AccListener, GyroListener 
                         + deviceHardwareAddress);
             }
         }
-        
+
         AcceptThread acceptThread = new AcceptThread();
         acceptThread.start();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 如果 textureView可用，就直接打开相机
+        if (textureView.isAvailable()) {
+            openCamera();
+        } else {
+            // 否则，就开启它的可用时监听。
+            textureView.setSurfaceTextureListener(surfaceTextureListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        // 先把相机的session关掉
+        if (cameraCaptureSession != null) {
+            cameraCaptureSession.close();
+        }
+        // 再关闭相机
+        if (null != opened_camera) {
+            opened_camera.close();
+        }
+        // 最后关闭ImageReader
+        if (null != imageReader) {
+            imageReader.close();
+        }
+        // 最后交给父View去处理
+        super.onPause();
+    }
+
+    private void openCamera() {
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);  // 初始化
+        cam_stateCallback = new CameraDevice.StateCallback() {
+            @Override
+            public void onOpened(@NonNull CameraDevice camera) {
+                opened_camera = camera;
+                try {
+                    requestBuilder = opened_camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                    requestBuilder.addTarget(texture_surface);
+                    request = requestBuilder.build();
+                    cam_capture_session_stateCallback = new CameraCaptureSession.StateCallback() {
+                        @Override
+                        public void onConfigured(@NonNull CameraCaptureSession session) {
+                            cameraCaptureSession = session;
+                            try {
+                                session.setRepeatingRequest(request, null, null);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                        }
+                    };
+                    opened_camera.createCaptureSession(
+                            Arrays.asList(texture_surface, imageReaderSurface)
+                            , cam_capture_session_stateCallback
+                            , null);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onDisconnected(@NonNull CameraDevice camera) {
+            }
+
+            @Override
+            public void onError(@NonNull CameraDevice camera, int error) {
+            }
+        };
+        checkPermission();
+        try {
+            cameraManager.openCamera(cameraManager.getCameraIdList()[0], cam_stateCallback, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkPermission() {
+        // 检查是否申请了权限
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this
+                    , Manifest.permission.CAMERA)) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA}, 1);
+            }
+        }
     }
 
     private static final int X = 0, Y = 1, Z = 2, TIMESTAMP = 3, NAME = 4, INDEX = 5;
@@ -135,37 +301,32 @@ public class MainActivity extends Activity implements AccListener, GyroListener 
 
     final private static Object accViewLock = new Object();
     final private static Object gyroViewLock = new Object();
+
     @Override
     public void accListener(float[] sensorData) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                DataPoint x, y, z;
-                x = new DataPoint(sensorData[INDEX] / 2, sensorData[X]);
-                y = new DataPoint(sensorData[INDEX] / 2, sensorData[Y]);
-                z = new DataPoint(sensorData[INDEX] / 2, sensorData[Z]);
+        runOnUiThread(() -> {
+            DataPoint x, y, z;
+            x = new DataPoint(sensorData[INDEX] / 2, sensorData[X]);
+            y = new DataPoint(sensorData[INDEX] / 2, sensorData[Y]);
+            z = new DataPoint(sensorData[INDEX] / 2, sensorData[Z]);
 
-                accDisplayBuf.get(0).appendData(x, true, MOTION_PREVIEW_SIZE);
-                accDisplayBuf.get(1).appendData(y, true, MOTION_PREVIEW_SIZE);
-                accDisplayBuf.get(2).appendData(z, true, MOTION_PREVIEW_SIZE);
-            }
+            accDisplayBuf.get(0).appendData(x, true, MOTION_PREVIEW_SIZE);
+            accDisplayBuf.get(1).appendData(y, true, MOTION_PREVIEW_SIZE);
+            accDisplayBuf.get(2).appendData(z, true, MOTION_PREVIEW_SIZE);
         });
     }
 
     @Override
     public void gyroListener(float[] sensorData) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                DataPoint x, y, z;
-                x = new DataPoint(sensorData[INDEX] / 2, sensorData[X]);
-                y = new DataPoint(sensorData[INDEX] / 2, sensorData[Y]);
-                z = new DataPoint(sensorData[INDEX] / 2, sensorData[Z]);
+        runOnUiThread(() -> {
+            DataPoint x, y, z;
+            x = new DataPoint(sensorData[INDEX] / 2, sensorData[X]);
+            y = new DataPoint(sensorData[INDEX] / 2, sensorData[Y]);
+            z = new DataPoint(sensorData[INDEX] / 2, sensorData[Z]);
 
-                gyroDisplayBuf.get(0).appendData(x, true, MOTION_PREVIEW_SIZE);
-                gyroDisplayBuf.get(1).appendData(y, true, MOTION_PREVIEW_SIZE);
-                gyroDisplayBuf.get(2).appendData(z, true, MOTION_PREVIEW_SIZE);
-            }
+            gyroDisplayBuf.get(0).appendData(x, true, MOTION_PREVIEW_SIZE);
+            gyroDisplayBuf.get(1).appendData(y, true, MOTION_PREVIEW_SIZE);
+            gyroDisplayBuf.get(2).appendData(z, true, MOTION_PREVIEW_SIZE);
         });
     }
 
@@ -191,6 +352,7 @@ public class MainActivity extends Activity implements AccListener, GyroListener 
         }
 
         boolean accConnected = false, gyroConnected = false;
+
         @Override
         public void run() {
             BluetoothSocket accSocket = null, gyroSocket = null;
